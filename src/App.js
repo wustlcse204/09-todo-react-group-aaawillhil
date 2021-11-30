d1dd7887341174f4ae3f3bd022869546dc2aeac5
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2021-11-29 23:58:44 -0600

    checking the todos work

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Todo from './Todo.js'
 import Newtodo from './Newtodo.js'
 import 'bootstrap/dist/css/bootstrap.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="container text-center">
-          <h2 className="my-5" id="title">Sahil and Will's TodoList</h2>
-          <div className="container mt-5">
-            <Newtodo />
-            <div className="todo-container">
-              <Todo />
-              <Todo />
-              <Todo />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+import axios from 'axios';
+let config = {
+  headers: {
+    "x-api-key": "27e349-06b730-3e1f43-19548a-c7735e"
   }
 }
 
-export default App;
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    let url = "https://cse204.work/todos";
+    axios.get(url, config).then(function (response) {
+      setTodos(response.data);
+    })
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container text-center">
+        <h2 className="my-5" id="title">Sahil and Will's TodoList</h2>
+        <div className="container mt-5">
+          <Newtodo />
+          <div className="todo-container">
+            {todos.map(todo => {
+              return <Todo key={todo.id} id={todo.id} bodyText={todo.text} completed={todo.completed} config={config} />
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
--- src/Todo.js
+++ src/Todo.js
@@ -1,23 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Todo.css';
-import { faCoffee, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
-class Todo extends Component {
-  render() {
-    return (
-      <div className="todo" id="${post.id}">
-        <input type="checkbox" className="checkbox" id="check-btn-${post.id}" />
-        <p className="todo-title">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        </p>
-        <button type="button" className="btn trash-btn ms-auto" id="trash-btn-${post.id}">
-          <FontAwesomeIcon icon={faTrash} size="lg" />
-        </button>
-      </div>
+export default function Todo(props) {
+  const [checked, setChecked] = useState(props.completed);
 
-    );
+  const handleTrash = () => {
+    console.log("trash");
   }
-}
 
-export default Todo;
+  const handleCheck = () => {
+    let url = `https://cse204.work/todos/${props.id}`;
+    let data = {
+      "completed": !checked
+    }
+    axios.put(url, data, props.config).then(function (response) {
+      console.log("Checked:", response.data);
+    });
+
+    setChecked(!checked);
+  }
+
+  return (
+    <div className="todo" id={props.id}>
+      <input
+        type="checkbox"
+        className="checkbox"
+        onChange={handleCheck}
+        defaultChecked={checked}
+      />
+      <p className={`todo-title ${(checked && "strike-through")}`}>
+        {props.bodyText}
+      </p>
+      <button type="button" className="btn trash-btn ms-auto" onClick={handleTrash}>
+        <FontAwesomeIcon icon={faTrash} size="lg" />
+      </button>
+    </div>
+  );
+}
