d85e8a1f4dfd60434b7fb45c38c91650160f10de
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2021-11-29 20:23:29 -0600

    fixed css

--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Todo.css';
+import { faCoffee, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 class Todo extends Component {
   render() {
     return (
       <div className="todo" id="${post.id}">
-        <input type="checkbox" className="checkbox" id="check-btn-${post.id}"></input>
+        <input type="checkbox" className="checkbox" id="check-btn-${post.id}" />
         <p className="todo-title">
-          {/* ${post.text} */}
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
         </p>
         <button type="button" className="btn trash-btn ms-auto" id="trash-btn-${post.id}">
-          <i className="fas fa-trash fa-2x"></i>
+          <FontAwesomeIcon icon={faTrash} size="lg" />
         </button>
       </div>
 
