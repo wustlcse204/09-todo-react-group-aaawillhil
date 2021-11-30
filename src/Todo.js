68f0fcf82196423f0b5873d2d3e279e8f0f39fcf
sahilpatel123 <sahilpatel@wustl.edu>
2021-11-29 18:07:30 -0600

    todo updated

--- src/Todo.js
+++ src/Todo.js
@@ -4,10 +4,10 @@ import './Todo.css';
 class Todo extends Component {
   render() {
     return (
-      <div class="todo">
+      <div class="todo" id="${post.id}">
         <input type="checkbox" class="checkbox" id="check-btn-${post.id}"></input>
         <p class ="todo-title">
-        do it
+        ${post.text}
         </p>
         <button type="button" class ="btn trash-btn ms-auto" id="trash-btn-${post.id}">
             <i class="fas fa-trash fa-2x"></i>
